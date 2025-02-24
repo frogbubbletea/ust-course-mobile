6796cb1c6b605d591f4e9065de1629601ee3e2c2
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2025-02-25 02:55:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/usthong/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.example.usthong
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.usthong.ui.theme.USThongTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            USThongTheme {
+                PrefixScreen("ACCT")
+            }
+        }
+    }
+}
+
+// Shows all courses under `prefix`
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PrefixScreen(prefix: String) {
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            LargeTopAppBar(
+                title = {
+                    Text(prefix)
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surface,
+                    scrolledContainerColor = MaterialTheme.colorScheme.surfaceContainerHigh
+                ),
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            contentPadding = PaddingValues(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(40) { index ->
+                CourseCard()
+            }
+        }
+    }
+}
+
+// Card showing info of a course
+@Composable
+fun CourseCard() {
+    Surface(
+        onClick = { },
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = MaterialTheme.shapes.small,
+        shadowElevation = 1.dp,
+        color = MaterialTheme.colorScheme.surfaceContainer,
+    ) {
+        Column (
+            modifier = Modifier
+                .padding(12.dp)
+        ) {
+            // Course code
+            Text(
+                text = "ACCT 1010",
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            // Course title
+            Text(
+                "Accounting, Business and Society．3 units"
+            )
+
+            Text("9 sections")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CourseCardPreview() {
+    USThongTheme {
+        CourseCard()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    USThongTheme {
+        PrefixScreen("ACCT")
+    }
+}
