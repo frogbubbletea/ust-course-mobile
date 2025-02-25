0bd2ab2c25a4383ad1b71aacf61f44025fb83370
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2025-02-26 03:11:19 +0800

    feat: fill in CourseCard layout

--- app/src/main/java/com/example/usthong/MainActivity.kt
+++ app/src/main/java/com/example/usthong/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.usthong
 
 import android.content.res.Configuration
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -18,30 +23,57 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text2.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.List
 import androidx.compose.material.icons.automirrored.rounded.List
 import androidx.compose.material.icons.rounded.List
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
+import androidx.compose.material3.LocalMinimumTouchTargetEnforcement
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.usthong.ui.theme.USThongTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -91,8 +123,16 @@ fun PrefixScreen(prefix: String) {
 }
 
 // Card showing info of a course
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun CourseCard() {
+    // Variables for section selection bottom sheet
+    val sections = listOf("L1 (1001)", "L2 (1002)", "L3 (1003)", "L4 (1004)", "L5 (1005)", "L6 (1006)", "L7 (1007)", "L8 (1008)", "L9 (1009)")
+    val sectionSheetState = rememberModalBottomSheetState()
+    val sectionSheetScope = rememberCoroutineScope()
+    var showSectionSheet by remember { mutableStateOf(false) }
+    var selectedSection by remember { mutableStateOf(sections[0]) }
+
     Surface(
         onClick = { },
         modifier = Modifier
@@ -112,67 +152,245 @@ fun CourseCard() {
             )
 
             // Course title
-            Text(
-                "Accounting, Business and Society"
-            )
+            Text("Accounting, Business and Society")
 
             Spacer(modifier = Modifier.height(8.dp))
 
+            // Quota of one selected section (total)
             Row(
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.Top
             ) {
-                // Number of sections
-                Surface(
-                    shape = MaterialTheme.shapes.small,
-                    color = MaterialTheme.colorScheme.surfaceContainerHigh
+                // Display of selected section
+                CompositionLocalProvider(
+                    LocalMinimumInteractiveComponentEnforcement provides false,
                 ) {
-                    Row(
-                        modifier = Modifier
-                            .padding(horizontal = 8.dp, vertical = 4.dp),
-                        verticalAlignment = Alignment.CenterVertically
+                    Surface(
+                        onClick = { showSectionSheet = true },
+                        color = Color.Transparent
                     ) {
-                        Icon(
-                            Icons.AutoMirrored.Outlined.List,
-                            contentDescription = stringResource(id = R.string.sections_icon_desc),
-                            modifier = Modifier.size(16.dp),
-                            tint = MaterialTheme.colorScheme.onSurfaceVariant
-                        )
-
-                        Spacer(modifier = Modifier.width(4.dp))
-
-                        Text(
-                            text = "9",
-                            color = MaterialTheme.colorScheme.onSurfaceVariant
-                        )
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(4.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = selectedSection,
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                            Icon(
+                                painter = painterResource(R.drawable.material_icon_dropdown),
+                                contentDescription = stringResource(id = R.string.sections_icon_desc),
+                                modifier = Modifier.size(16.dp),
+                                tint = MaterialTheme.colorScheme.primary
+                            )
+                        }
                     }
                 }
 
-                // Number of units
-                Surface(
-                    shape = MaterialTheme.shapes.small,
-                    color = MaterialTheme.colorScheme.surfaceContainerHigh
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .padding(horizontal = 8.dp, vertical = 4.dp),
-                        verticalAlignment = Alignment.CenterVertically
+                // Section selection bottom sheet
+                if (showSectionSheet) {
+                    ModalBottomSheet(
+                        onDismissRequest = {
+                            showSectionSheet = false
+                        },
+                        sheetState = sectionSheetState
                     ) {
-                        Icon(
-                            painter = painterResource(R.drawable.material_icon_units),
-                            contentDescription = stringResource(id = R.string.units_icon_desc),
-                            modifier = Modifier.size(16.dp),
-                            tint = MaterialTheme.colorScheme.onSurfaceVariant
+                        // Title of section selection bottom sheet
+                        Text(
+                            text = "Select section",
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            style = MaterialTheme.typography.titleLarge,
+                            textAlign = TextAlign.Center
                         )
 
-                        Spacer(modifier = Modifier.width(4.dp))
+                        Spacer(modifier = Modifier.height(16.dp))
 
-                        Text(
-                            text = "3 units",
-                            color = MaterialTheme.colorScheme.onSurfaceVariant
+                        // Add sections to the sheet one by one
+                        sections.forEach {
+                            Surface(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                color = if (it == selectedSection) MaterialTheme.colorScheme.surfaceContainerHigh else Color.Transparent,
+                                onClick = {
+                                    selectedSection = it
+                                    sectionSheetScope.launch { sectionSheetState.hide() }.invokeOnCompletion {
+                                        if (!sectionSheetState.isVisible) {
+                                            showSectionSheet = false
+                                        }
+                                    }
+                                }
+                            ) {
+                                Text(
+                                    text = it,
+                                    modifier = Modifier
+                                        .padding(horizontal = 32.dp, vertical = 12.dp)
+                                )
+                            }
+                        }
+                    }
+                }
+
+                // "Quota" column
+                Column(
+                    modifier = Modifier.weight(.25f),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Quota",
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                    Text(
+                        text = "120",
+                        style = MaterialTheme.typography.headlineMedium,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+
+                // "Enrol" column
+                Column(
+                    modifier = Modifier.weight(.25f),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Enrol",
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                    Text(
+                        text = "101",
+                        style = MaterialTheme.typography.headlineMedium,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+
+                // "Avail" column
+                Column(
+                    modifier = Modifier.weight(.25f),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Avail",
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                    Text(
+                        text = "19",
+                        style = MaterialTheme.typography.headlineMedium,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+
+                // "Wait" column
+                Column(
+                    modifier = Modifier.weight(.25f),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Wait",
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                    Text(
+                        text = "0",
+                        style = MaterialTheme.typography.headlineMedium,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            CompositionLocalProvider(  // Remove hardcoded padding around button
+                LocalMinimumInteractiveComponentEnforcement provides false,
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    // Number of sections
+                    Surface(
+                        shape = MaterialTheme.shapes.small,
+                        color = MaterialTheme.colorScheme.surfaceContainerHigh,
+                        onClick = { }
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .padding(horizontal = 8.dp, vertical = 4.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                Icons.AutoMirrored.Outlined.List,
+                                contentDescription = stringResource(id = R.string.sections_icon_desc),
+                                modifier = Modifier.size(16.dp),
+                                tint = MaterialTheme.colorScheme.onSurfaceVariant
+                            )
+
+                            Spacer(modifier = Modifier.width(4.dp))
+
+                            Text(
+                                text = "9 sections",
+                                color = MaterialTheme.colorScheme.onSurfaceVariant
+                            )
+                        }
+                    }
+
+                    // Number of units
+                    Surface(
+                        shape = MaterialTheme.shapes.small,
+                        color = MaterialTheme.colorScheme.surfaceContainerHigh
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .padding(horizontal = 8.dp, vertical = 4.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                painter = painterResource(R.drawable.material_icon_units),
+                                contentDescription = stringResource(id = R.string.units_icon_desc),
+                                modifier = Modifier.size(16.dp),
+                                tint = MaterialTheme.colorScheme.onSurfaceVariant
+                            )
+
+                            Spacer(modifier = Modifier.width(4.dp))
+
+                            Text(
+                                text = "3 units",
+                                color = MaterialTheme.colorScheme.onSurfaceVariant
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    // Star button
+                    IconButton(
+                        modifier = Modifier.
+                            then(Modifier.size(28.dp)),
+                        onClick = { }
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.material_icon_star),
+                            contentDescription = stringResource(id = R.string.star_icon_desc),
+                            tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                            modifier = Modifier.size(28.dp)
+                        )
+                    }
+
+                    // Open in Class Schedule & Quota button
+                    IconButton(
+                        modifier = Modifier.
+                        then(Modifier.size(28.dp)),
+                        onClick = { }
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.material_icon_open_in_browser),
+                            contentDescription = stringResource(id = R.string.open_in_ust_icon_desc),
+                            tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                            modifier = Modifier.size(28.dp)
                         )
                     }
                 }
             }
+
         }
     }
 }
