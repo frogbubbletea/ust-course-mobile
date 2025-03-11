adc8a351d6389584892996c15831adcd4dbfb223
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2025-03-12 02:46:22 +0800

    build: release 0.1.0-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,19 +17,23 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
+            isDebuggable = false
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
             signingConfig = signingConfigs.getByName("debug")
+        }
+        debug {
+            isDebuggable = true
         }
     }
     compileOptions {
