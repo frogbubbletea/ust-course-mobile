6559df1b7af7c82f628641d76ae6665f053e4f67
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2025-05-12 03:00:37 +0800

    build: bump version to 0.3.0-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.frogbubbletea.ustcoursemobile"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "0.3.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -32,6 +32,8 @@ android {
                 "proguard-rules.pro"
             )
             signingConfig = signingConfigs.getByName("debug")
+
+            isDebuggable = false
         }
     }
     compileOptions {
